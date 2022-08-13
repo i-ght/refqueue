585c6ade9c0ab0d6034e2c6ea861561489e4f34c
i-ght <toph.ght@gmail.com>
2022-08-13 07:15:48 -0600

    Program, deprogram, reprogram (How's your funk-entelechy?)

--- program/program.c
+++ program/program.c
@@ -1,4 +1,31 @@
+#include "refqueue.h"
+
+enum {OK=0,ERR=-1};
+
+
 int main(void)
 {
+    struct RefQueue ref_q = {0};
+    if (ERR == ref_q_construct(&ref_q, 100, malloc)) {
+        return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < 100; i++) {
+        if (ERR == ref_q_enqueue(&ref_q, "hello world")) {
+            return EXIT_FAILURE;
+        }
+    }
+
+    for (size_t i = 0; i < 100; i++) {
+        char* value = NULL;
+        if (ERR == ref_q_dequeue(&ref_q, (void*)&value)) {
+            return EXIT_FAILURE;
+        }
+
+        int _ = printf("got value from queue: %s\n", value);
+
+
+    }
+
     return 0;
 }
